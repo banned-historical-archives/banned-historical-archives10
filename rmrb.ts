32e00039dfd60c3da18a7b708892f2d7c933aa80
Your Name <you@example.com>
2024-04-11 22:15:07 +0800

    rmrb

---
+++ rmrb.ts
@@ -0,0 +1,16 @@
+export default {
+    "entity": {
+      "id": "rmrb",
+      "name": "人民日报",
+      "internal": false,
+      "official": false,
+      "type": "db",
+      "author": "",
+      "files": []
+    },
+    "parser_option": {},
+    "parser_id": "rmrb",
+    "path": "selected",
+    "resource_type": "book",
+    "version": 2
+  }
