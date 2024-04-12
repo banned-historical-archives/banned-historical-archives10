31b4ae3e7b819e2746ab49de553709e8f30d1207
Your Name <you@example.com>
2024-04-13 00:09:29 +0800

    update

--- build-selected.js
+++ build-selected.js
@@ -1,12 +1,30 @@
 const fs = require('fs-extra');
 const { join } = require('path');
 
-const whitelist = ['张春桥','江青','王洪文','姚文元','毛远新', '毛泽东'];
-function includes(str, arr) {
+const keywords = ['张春桥','江青','王洪文','姚文元','毛远新', '毛泽东', '毛主席', '四人帮'];
+const keywords_special = [
+  {year_start: 1958, year_end: 1981, keyword: '邓小平'},
+  {year_start: 1958, year_end: 1981, keyword: '刘少奇'},
+  {year_start: 1958, year_end: 1961, keyword: '彭德怀'},
+  {year_start: 1950, year_end: 1981, keyword: '戚本禹'},
+
+  {year_start: 1966, year_end: 1983, keyword: '揭发'},
+  {year_start: 1966, year_end: 1978, keyword: '彭真'},
+  {year_start: 1966, year_end: 1978, keyword: '华国锋'},
+  {year_start: 1966, year_end: 1978, keyword: '汪东兴'},
+  {year_start: 1966, year_end: 1978, content_keyword: '邓榕'},
+  {year_start: 1966, year_end: 1978, content_keyword: '邓朴方'},
+
+
+];
+
+function normalize(str) {
   str = str.replace(/松江青[浦年]/g, '').replace(/[浙镇吴隆黄]江青/g, '');
+  return str;
+}
+function includes(str, arr) {
   for (const i of arr) {
       if (str.indexOf(i) >= 0) {
-          console.log('#', i)
           return true;
       }
   }
@@ -22,6 +40,7 @@ function includes(str, arr) {
   // const range = [[1946,5], [1946,12]];
 
   await fs.ensureDir(join(__dirname, './selected'));
+  let total = 0;
   for (let year = range[0][0]; year <= range[1][0]; ++year) {
     console.log(year);
     await fs.ensureDir(join(__dirname, './selected/' + year));
@@ -35,13 +54,30 @@ function includes(str, arr) {
       for (const i of await fs.readdir(
         join(__dirname, `./json/${year}/${month}`),
       )) {
-        const content = JSON.parse((
+        const json = JSON.parse((
           await fs.readFile(join(__dirname, `./json/${year}/${month}/${i}`))
         ).toString());
-        if (includes(content.title, [...whitelist, '毛主席']) || content.authors.reduce((m,k) => {
-          return m || whitelist.includes(k)
-        }, false)) {
-          console.log(content.title)
+        const normalized_title = normalize(json.title)
+        if (
+          includes(normalized_title, keywords) ||
+          json.authors.reduce((m,k) => {
+            return m || keywords.includes(k)
+          }, false) ||
+          keywords_special.reduce((m, k) => {
+            return m || (
+              k.year_start >= json.dates[0].year && k.year_end <= json.dates[0].year && (
+                k.keyword ? (
+                  normalized_title.indexOf(k.keyword) >= 0 ||
+                  json.authors.reduce((m,a) => {
+                    return m || a == k.keyword
+                  }, false)
+                ) : normalized_content.indexOf(k.content_keyword) >= 0
+              )
+            )
+          }, false)
+        ) {
+          // console.log(json.title)
+          ++total;
           await fs.copyFile(
             join(__dirname, `./json/${year}/${month}/${i}`),
             join(__dirname, `./selected/${year}/${month}/${i}`),
@@ -50,4 +86,5 @@ function includes(str, arr) {
       }
     }
   }
+  console.log(total)
 })();
